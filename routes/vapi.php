f6265ea5e3da5ab75e4aef71003078a6572e5b02
unknown <hello.safrian@gmail.com>
2024-01-04 14:46:55 +0700

    fkjfwse

--- routes/vapi.php
+++ routes/vapi.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Vendors\UserController;
 use App\Http\Controllers\Vendors\PerusahaanController;
+use App\Http\Controllers\Vendors\TenderController;
 use App\Http\Controllers\Vendors\JajaranController;
 use App\Http\Controllers\Vendors\AktaController;
 use App\Http\Controllers\Vendors\FileController;
