a50d1f8fa72b580525340b9daf0c1719378a77dd
unknown <hello.safrian@gmail.com>
2024-01-14 20:56:58 +0700

    sdljknsd

--- app/Http/Controllers/Vendors/Admin/ATenderController.php
+++ app/Http/Controllers/Vendors/Admin/ATenderController.php
@@ -79,10 +79,12 @@ class ATenderController extends Controller
         $td['kbli_list'] = [];
         if(count($kbliList) > 0){
             for ($i=0; $i < count($kbliList); $i++) { 
-                $td['kbli_list'] = [
+                $l[$i] = [
                     'value' => $kbliList[$i]->nomor_kbli,
                     'label' => $kbliList[$i]->nama_kbli
                 ];
+
+                array_push($td['kbli_list'], $l[$i]);
             }
         }
         
