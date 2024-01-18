a52a65a98ff35059bd0c49337a155ba34a92e0ec
sogatanco <adeliapucha46@gmail.com>
2024-01-18 11:59:19 +0700

    ssdgsdg

--- app/Http/Controllers/Vendors/TenderController.php
+++ app/Http/Controllers/Vendors/TenderController.php
@@ -25,14 +25,28 @@ class TenderController extends Controller
             ->orWhere('metode_pengadaan', 'terbatas')
             ->get();
 
-            foreach($data as $d){
-               if(count(TenderPeserta::where('tender_id', $d->id_tender)->where('perusahaan_id',  ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id)->get())>0){
-                $d->register=true;
-                $d->status_peserta=TenderPeserta::where('tender_id', $d->id_tender)->where('perusahaan_id',  ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id)->first()->status;
-               }else{
-                $d->register=false;
-               }
+        foreach ($data as $d) {
+            if (count(TenderPeserta::where('tender_id', $d->id_tender)->where('perusahaan_id',  ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id)->get()) > 0) {
+                $d->register = true;
+                $statusPeserta = TenderPeserta::where('tender_id', $d->id_tender)->where('perusahaan_id',  ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id)->first()->status;
+                if (count(TenderPeserta::where('tender_id', $d->id_tender)->where('status',  'pemenang')->get()) > 0) {
+                    if ($statusPeserta == 'lulus_tahap_1' || $statusPeserta == 'submit_dokumen') {
+                        $d->status_peserta = 'gagal_menang';
+                    } else {
+                        $d->status_peserta = $statusPeserta;
+                    }
+                } else if(count(TenderPeserta::where('tender_id', $d->id_tender)->where('status',  'lulus_tahap_1')->get()) > 0){
+                    if ($statusPeserta == 'submit_dokumen') {
+                        $d->status_peserta = 'tidak lulus tahap 1';
+                    } else {
+                        $d->status_peserta = $statusPeserta;
+                    }
+                }
+            } else {
+                $d->register = false;
+                $d->status_peserta = '';
             }
+        }
 
         return response()->json([
             "success" => true,
@@ -42,14 +56,14 @@ class TenderController extends Controller
 
     public function ikot(Request $request)
     {
-        
-        if( count(TenderPeserta::where('tender_id', $request->tender_id)->where('perusahaan_id', ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id)->get())>0){
-          
-                return response()->json([
-                    "success" => false,
-                    "message"=>"sudah terdaftar"
-                ], 409);
-        }else{
+
+        if (count(TenderPeserta::where('tender_id', $request->tender_id)->where('perusahaan_id', ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id)->get()) > 0) {
+
+            return response()->json([
+                "success" => false,
+                "message" => "sudah terdaftar"
+            ], 409);
+        } else {
             $t = new TenderPeserta();
             $t->perusahaan_id = ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id;
             $t->tender_id = $request->tender_id;
@@ -59,52 +73,53 @@ class TenderController extends Controller
                 ], 200);
             }
         }
-       
     }
 
-    public function showTender($slug){
-        $d=Tender::where('slug', $slug)->first();
+    public function showTender($slug)
+    {
+        $d = Tender::where('slug', $slug)->first();
         return response()->json([
             "success" => true,
-            "data"=>$d
+            "data" => $d
         ], 200);
     }
 
     public function pesertaTender($slug)
-    {   
+    {
         $company = Perusahaan::where('user_id', Auth::user()->id)->first();
         $data  = Tender::where(['tender.slug' => $slug, 'tender_peserta.perusahaan_id' => $company->id])
-                    ->leftJoin('tender_peserta', 'tender_peserta.tender_id', '=', 'tender.id_tender')
-                    ->first();
-                
+            ->leftJoin('tender_peserta', 'tender_peserta.tender_id', '=', 'tender.id_tender')
+            ->first();
+
         return response()->json([
             "success" => true,
-            "data"=>$data
+            "data" => $data
         ], 200);
     }
 
     public function upload(Request $request)
-    {   
+    {
         $t = TenderPeserta::where('tender_id', $request->tender_id)->where('perusahaan_id', ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id)->first();
-        
+
         Storage::disk('public_vendor')->put('tender/' . $request->tender_id . '/' . ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id . '/' . $request->key . '.pdf', base64_decode($request->file, true));
         $t[$request->key] = 'tender/' . $request->tender_id . '/' . ViewPerusahaan::where('user_id', Auth::user()->id)->get()->first()->id . '/' . $request->key . '.pdf';
-        
+
         if ($t->save()) {
             return response()->json([
                 "success" => true,
-                "message"=>$request->key. "file sudah di upload"
+                "message" => $request->key . "file sudah di upload"
             ], 200);
         }
     }
 
-    public function finalIkot($id){
-        $tender=Tender::find($id);
-        $docs=json_decode($tender->centang_dok_wajib);
-        
+    public function finalIkot($id)
+    {
+        $tender = Tender::find($id);
+        $docs = json_decode($tender->centang_dok_wajib);
+
         return response()->json([
             "success" => true,
-            "data"=>$docs
+            "data" => $docs
         ], 200);
     }
 
@@ -112,7 +127,7 @@ class TenderController extends Controller
     {
         $tender = TenderPeserta::find($idPeserta);
 
-        if(!$tender){
+        if (!$tender) {
             throw new HttpResponseException(response([
                 "status" => false,
                 "message" => "Participant data not found."
@@ -121,12 +136,12 @@ class TenderController extends Controller
 
         $tender->status = 'submit_dokumen';
 
-        if($tender->save()){
+        if ($tender->save()) {
             return response()->json([
                 "status" => true,
                 "message" => "Tender documents have been submitted."
             ], 200);
-        }else{
+        } else {
             throw new HttpResponseException(response([
                 "status" => false,
                 "message" => "Something went wrong."
