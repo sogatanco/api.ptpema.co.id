e671223ec4c3af10dc86d4ea44fefa9078c0e69b
unknown <hello.safrian@gmail.com>
2024-02-02 11:42:16 +0700

    dslds

--- app/Http/Controllers/Projects/ProjectController.php
+++ app/Http/Controllers/Projects/ProjectController.php
@@ -341,7 +341,7 @@ class ProjectController extends Controller
                 'desc' => $request->desc,
                 'start_date' => $request->start_date,
                 'end_date' => $request->end_date,
-                'partner' => $request->partner
+                'partner' => $request->partner_id
             ];
 
             $stageUpdated = ProjectStage::where('id', $request->stage_id)->update($stage);
