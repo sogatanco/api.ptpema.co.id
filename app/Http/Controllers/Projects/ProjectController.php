20e2dc816ab9530ec9ec9f9fd9be1956c48c65c5
unknown <hello.safrian@gmail.com>
2024-02-05 10:12:40 +0700

    ksjhd

--- app/Http/Controllers/Projects/ProjectController.php
+++ app/Http/Controllers/Projects/ProjectController.php
@@ -999,15 +999,11 @@ class ProjectController extends Controller
         for ($i=0; $i < count($projects); $i++) { 
             // cari task by project dan employe
             $where = ['project_task_pics.project_id' => $projects[$i]->project_id, 'project_task_pics.employe_id' => $employeId];
-            $tasks[$i] = TaskPic::select('task_parent', 'task_title', 'start_date', 'end_date')
+            $projects[$i]['tasks'] = TaskPic::select('task_parent', 'task_title', 'start_date', 'end_date')
                                     ->where($where)
                                     ->join('task_latest_status', 'task_latest_status.task_id', '=', 'project_task_pics.task_id')
                                     ->get();
 
-
-            if(count($tasks[$i]) > 0){
-                $projects[$i]['tasks'] = $tasks[$i];
-            }
         }
 
         return response()->json([
