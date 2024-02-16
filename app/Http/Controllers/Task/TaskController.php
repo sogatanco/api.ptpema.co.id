64715d51fae64c3330ffa3f1a42be85acad414e4
unknown <hello.safrian@gmail.com>
2024-02-16 09:51:53 +0700

    haha

--- app/Http/Controllers/Task/TaskController.php
+++ app/Http/Controllers/Task/TaskController.php
@@ -268,66 +268,64 @@ class TaskController extends Controller
 
         // cari dulu dari table pic
         $taskByPic = TaskPic::where(['project_id' => $projectId, 'employe_id' => $employeId])
-                ->get();
+                    ->get();
 
         $taskIds = [];
         for ($ti=0; $ti < count($taskByPic); $ti++) { 
-        $taskIds[] = $taskByPic[$ti]->task_id;
+            $taskIds[] = $taskByPic[$ti]->task_id;
         };
 
         $tasks = TaskStatus::select('task_id', 'task_parent')
-            ->whereIn('task_id', $taskIds)
-            ->get();
+                ->whereIn('task_id', $taskIds)
+                ->get();
 
         $parentTasks =[];
         $parentSubtasks=[];
         for ($p=0; $p < count($tasks); $p++) { 
-        if($tasks[$p]->task_parent === null){
-            $parentTasks[] = $tasks[$p]->task_id;
-        }else{
-            if(!in_array($tasks[$p]->task_parent, $parentTasks)){
-                $parentSubtasks[] = $tasks[$p]->task_parent;
+            if($tasks[$p]->task_parent === null){
+                $parentTasks[] = $tasks[$p]->task_id;
+            }else{
+                if(!in_array($tasks[$p]->task_parent, $parentTasks)){
+                    $parentSubtasks[] = $tasks[$p]->task_parent;
+                }
             }
-        }
         }
 
         // jika parent cari subtask
         $pic_parent =[];
         if(count($parentTasks) > 0){
-        $pic_parent = TaskStatus::whereIn('task_id', $parentTasks)
-                        ->get();
+            $pic_parent = TaskStatus::whereIn('task_id', $parentTasks)
+                            ->get();
 
-        for ($pp=0; $pp < count($pic_parent); $pp++) { 
-            $pic_parent[$pp]['comments'] = Comment::where('task_id', $pic_parent[$pp]->task_id)->count();
-            
-            $pic_parent[$pp]['files'] = TaskFile::select('file_id', 'file_name')
+            for ($pp=0; $pp < count($pic_parent); $pp++) { 
+                $pic_parent[$pp]['comments'] = Comment::where('task_id', $pic_parent[$pp]->task_id)->count();
+                
+                $pic_parent[$pp]['files'] = TaskFile::select('file_id', 'file_name')
+                                                ->where('task_id', $pic_parent[$pp]->task_id)
+                                                ->get(); 
+
+                $pic_parent[$pp]['subtasks'] = TaskStatus::where([
+                                                    'task_parent' => $pic_parent[$pp]->task_id,
+                                                ])
+                                                ->select('task_latest_status.*', TaskStatus::raw('(SELECT COUNT(*) FROM comments WHERE comments.task_id = task_latest_status.task_id) AS comments'))
+                                                ->get() ;
+
+                $pic_parent[$pp]['pics'] = TaskPic::select('project_task_pics.id', 'project_task_pics.employe_id', 'employees.first_name')
                                             ->where('task_id', $pic_parent[$pp]->task_id)
-                                            ->get(); 
+                                            ->join('employees', 'employees.employe_id','=','project_task_pics.employe_id')
+                                            ->get();
+                                                
+                for ($stf1=0; $stf1 < count($pic_parent[$pp]['subtasks']); $stf1++) { 
+                    $pic_parent[$pp]['subtasks'][$stf1]['files'] = TaskFile::select('file_id', 'file_name')
+                                                                    ->where('task_id', $pic_parent[$pp]['subtasks'][$stf1]->task_id)
+                                                                    ->get();
 
-            $pic_parent[$pp]['subtasks'] = TaskStatus::where([
-                                                'task_parent' => $pic_parent[$pp]->task_id,
-                                            ])
-                                            ->select('task_latest_status.*', TaskStatus::raw('(SELECT COUNT(*) FROM comments WHERE comments.task_id = task_latest_status.task_id) AS comments'))
-                                            ->get() ;
-
-            $pic_parent[$pp]['pics'] = TaskPic::select('project_task_pics.id', 'project_task_pics.employe_id', 'employees.first_name')
-                                        ->where('task_id', $pic_parent[$pp]->task_id)
-                                        ->join('employees', 'employees.employe_id','=','project_task_pics.employe_id')
-                                        ->get();
-                                            
-            for ($stf1=0; $stf1 < count($pic_parent[$pp]['subtasks']); $stf1++) { 
-                $pic_parent[$pp]['subtasks'][$stf1]['files'] = TaskFile::select('file_id', 'file_name')
-                                                                ->where('task_id', $pic_parent[$pp]['subtasks'][$stf1]->task_id)
-                                                                ->get();
-
-                $pic_parent[$pp]['subtasks'][$stf1]['pics'] =  TaskPic::select('project_task_pics.id', 'project_task_pics.employe_id', 'employees.first_name')
-                                                                ->where('task_id', $pic_parent[$pp]['subtasks'][$stf1]->task_id)
-                                                                ->join('employees', 'employees.employe_id','=','project_task_pics.employe_id')
-                                                                ->get();
-            }                                   
-
-        }
-
+                    $pic_parent[$pp]['subtasks'][$stf1]['pics'] =  TaskPic::select('project_task_pics.id', 'project_task_pics.employe_id', 'employees.first_name')
+                                                                    ->where('task_id', $pic_parent[$pp]['subtasks'][$stf1]->task_id)
+                                                                    ->join('employees', 'employees.employe_id','=','project_task_pics.employe_id')
+                                                                    ->get();
+                }                                   
+            }
         }
 
         // jika subtask cari parent
@@ -1110,6 +1108,25 @@ class TaskController extends Controller
     // 3 LEVEL TASK
     public function projectTaskByEmploye($projectId, $employeId)
     {
+
+        $employeId = Employe::employeId();
+
+        // cari divisi aktif
+        $employeDivision = ProjectHistory::select('employe_id')
+                        ->where(['project_id' => $projectId, 'active' => 1])
+                        ->first();
+
+        if($employeId !== $employeDivision->employe_id){
+            $employeCompare = Structure::select('organization_id')
+                            ->whereIn('employe_id', [$employeDivision->employe_id, $employeId])
+                            ->get();
+
+            $isMemberActive = $employeCompare[0]->organization_id === $employeCompare[1]->organization_id;
+        } else {
+            // jika user active adalah manager
+            $isMemberActive = true;
+        }
+
         $level1 = TaskStatus::where(['project_id' => $projectId, 'employe_id' => $employeId, 'task_parent' => null])
                 ->get();
 
@@ -1130,6 +1147,7 @@ class TaskController extends Controller
         return response()->json([
             "status" => true,
             "total" => count($level1),
+            "is_member_active" => $isMemberActive,
             "data" => $level1
         ], 200);
     }
