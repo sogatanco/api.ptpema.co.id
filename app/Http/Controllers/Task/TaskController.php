b0a695d79ba14d1548cd65088a91962fc6d52482
unknown <hello.safrian@gmail.com>
2024-02-20 23:04:20 +0700

    update task controller

--- app/Http/Controllers/Task/TaskController.php
+++ app/Http/Controllers/Task/TaskController.php
@@ -1166,7 +1166,13 @@ class TaskController extends Controller
             $parents = TaskStatus::whereIn('task_latest_status.task_id', $parentIds)
                     ->leftJoin('task_latest_status as level2', 'task_latest_status.task_parent', '=', 'level2.task_id')
                     ->leftJoin('task_latest_status as level1', 'level1.task_id', '=', 'level2.task_parent')
-                    ->select('task_latest_status.task_id as level_3_id', 'level2.task_id as level_2_id', 'level1.task_id as level_1_id')
+                    ->select(
+                        'task_latest_status.task_id as level_3_id', 
+                        'task_latest_status.task_parent as level_3_id_parent', 
+                        'level2.task_id as level_2_id', 
+                        'level2.task_parent as level_2_id_parent', 
+                        'level1.task_id as level_1_id'
+                    )
                     ->get();
 
             for ($p=0; $p < count($parents); $p++) { 
