5824348fdfaa454f24984578de3efc8e184c98db
unknown <hello.safrian@gmail.com>
2024-03-06 21:07:43 +0700

    sdlguhsd

--- app/Http/Controllers/Projects/ProjectController.php
+++ app/Http/Controllers/Projects/ProjectController.php
@@ -977,7 +977,7 @@ class ProjectController extends Controller
             "projIds" => $projectIds,
             "total" => count($projects),
             "data" => $projects,
-            "somplak" => $somplak[0]->category
+            "somplak" => $somplak
         ],200, [], JSON_NUMERIC_CHECK);
     }
     // OLD FUNCTION projectByEmployeDivision
