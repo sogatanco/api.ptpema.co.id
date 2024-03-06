00509b90f51ca0368badcdf0f793bbd6f6ac4fbe
unknown <hello.safrian@gmail.com>
2024-03-06 15:01:53 +0700

    update recent update function in project controller

--- app/Http/Controllers/Projects/ProjectController.php
+++ app/Http/Controllers/Projects/ProjectController.php
@@ -1238,12 +1238,12 @@ class ProjectController extends Controller
     public function recentUpdate()
     {
         $data = TaskStatus::where('division', 21)
-                // ->groupBy('project_id')
                 ->orderBy('updated_at', 'DESC')
+                ->groupBy('project_id')
                 ->get();
 
         return response()->json([
-            "status" => "dfgdfg",
+            "status" => "heheheheh",
             "total" => count($data),
             "data" => $data
         ], 200, [], JSON_NUMERIC_CHECK);
