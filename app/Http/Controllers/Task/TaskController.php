f9f2cbfee72735892fca78c0bcca6e07b36c8ade
unknown <hello.safrian@gmail.com>
2024-03-09 15:45:19 +0700

    update add sub function

--- app/Http/Controllers/Task/TaskController.php
+++ app/Http/Controllers/Task/TaskController.php
@@ -1426,13 +1426,14 @@ class TaskController extends Controller
         if($current->sub !== null){
             $subArr = json_decode($current->sub);
             $reqSub[0]['id'] = count($subArr) + 1;
+            $reqSub[0]['employe_id'] = $employeId;
             array_push($subArr, $reqSub[0]);
         }else{
             $reqSub[0]['id'] = 1;
+            $reqSub[0]['employe_id'] = $employeId;
             $subArr = $reqSub;
         }
 
-        $subArr[0]['employe_id'] = $employeId;
 
         $data = json_encode($subArr);
                 
