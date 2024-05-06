c8ebf4cddaf9d05804bf4f931c693901d29ed08b
unknown <hello.safrian@gmail.com>
2024-05-06 11:01:43 +0700

    update store general data function

--- app/Http/Controllers/Vendors/PerusahaanController.php
+++ app/Http/Controllers/Vendors/PerusahaanController.php
@@ -93,10 +93,6 @@ class PerusahaanController extends Controller
 
     public function store(Request $request)
     {
-        return response()->json([
-            "message" => "from store endpoint"
-        ], 200);
-
         $data = $request->all();
 
         $validator = Validator::make($request->all(), [
