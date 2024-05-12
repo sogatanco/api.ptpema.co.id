e0e7c31eb85bcce5e9fb8508e61945bca2332a1e
unknown <hello.safrian@gmail.com>
2024-05-12 15:53:47 +0700

    fixing bug in public user controller

--- app/Http/Controllers/Vendors/PublicUserController.php
+++ app/Http/Controllers/Vendors/PublicUserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Vendors;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use App\Mail\InfoToVendor;
+use Mail;
 
 class PublicUserController extends Controller
 {
