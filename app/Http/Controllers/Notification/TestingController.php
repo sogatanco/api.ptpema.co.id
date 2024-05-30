fb6912aef35f6199475018e8541ca895b4e46f1f
unknown <hello.safrian@gmail.com>
2024-05-30 15:24:03 +0700

    fixing bug in notification controller

--- app/Http/Controllers/Notification/TestingController.php
+++ app/Http/Controllers/Notification/TestingController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Notification;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Notification\NotificationController;
-use Illuminate\Support\Facades\Auth;
 
 class TestingController extends Controller
 {
