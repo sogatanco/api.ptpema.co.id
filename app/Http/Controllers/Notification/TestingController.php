d1fd601b41bb99059ae607f50b70ea561d178291
unknown <hello.safrian@gmail.com>
2024-05-30 15:13:08 +0700

    testing notification function

--- app/Http/Controllers/Notification/TestingController.php
+++ app/Http/Controllers/Notification/TestingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Notification;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Controller\Notification\NotificationController;
+use App\Http\Controllers\Notification\NotificationController;
 use Illuminate\Support\Facades\Auth;
 
 class TestingController extends Controller
