fbcbd5aaad495bba20c6f7656ef1f94d8b3e34fc
unknown <hello.safrian@gmail.com>
2024-06-03 15:23:25 +0700

    create notification feature

--- app/Http/Controllers/Notification/TestingController.php
+++ app/Http/Controllers/Notification/TestingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Notification;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Notification;
+use App\Http\Controllers\NotificationController;
 
 class TestingController extends Controller
 {
@@ -14,8 +14,8 @@ class TestingController extends Controller
         $recipient = '20230977K';
         $entityId = 1;
         $url = 'https://www.google.com';
-        
-        new Notification('CREATE_TASK', $resipient, $entityId, $url);
+
+        new NotificationController('CREATE_TASK', $resipient, $entityId, $url);
 
         return response()->json([
             'data' => "berhasil menyimpan notifikasi"
