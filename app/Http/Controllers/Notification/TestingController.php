2328a56031d65c6fcdd5db54cd65217a97bf668e
unknown <hello.safrian@gmail.com>
2024-06-03 15:50:47 +0700

    testing notif

--- app/Http/Controllers/Notification/TestingController.php
+++ app/Http/Controllers/Notification/TestingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Notification;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\Notification\NotificationController;
 
 class TestingController extends Controller
 {
