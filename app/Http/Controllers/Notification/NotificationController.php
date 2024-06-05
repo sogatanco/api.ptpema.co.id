c9d0b759edf17a4fec667e9e191abf22464eb456
unknown <hello.safrian@gmail.com>
2024-06-05 12:14:36 +0700

    testing taging task notification

--- app/Http/Controllers/Notification/NotificationController.php
+++ app/Http/Controllers/Notification/NotificationController.php
@@ -35,25 +35,25 @@ class NotificationController extends Controller
         $sent = [];
 
         // save notification
-        if(is_array($recipientArray)){
-            for ($r=0; $r < count($recipientArray); $r++) { 
-                if($employe->employe_id !== $recipientArray[$r]['employe_id']){
-                    if(!in_array($recipientArray[$r]['employe_id'], $sent)){
-                        $data = [
-                            'actor' => $employe->employe_id,
-                            'recipient' => $recipientArray[$r]['employe_id'],
-                            'entity_type_id' => $entityTypeId,
-                            'entity_id' => $entityId,
-                        ];
+        // if(is_array($recipientArray)){
+        //     for ($r=0; $r < count($recipientArray); $r++) { 
+        //         if($employe->employe_id !== $recipientArray[$r]['employe_id']){
+        //             if(!in_array($recipientArray[$r]['employe_id'], $sent)){
+        //                 $data = [
+        //                     'actor' => $employe->employe_id,
+        //                     'recipient' => $recipientArray[$r]['employe_id'],
+        //                     'entity_type_id' => $entityTypeId,
+        //                     'entity_id' => $entityId,
+        //                 ];
             
-                        $newNotification = new Notification($data);
-                        $newNotification->save();
+        //                 $newNotification = new Notification($data);
+        //                 $newNotification->save();
         
-                        array_push($sent, $recipientArray[$r]['employe_id']);
-                    }
-                }
-            }
-        }
+        //                 array_push($sent, $recipientArray[$r]['employe_id']);
+        //             }
+        //         }
+        //     }
+        // }
 
         return $recipientArray;
     }
