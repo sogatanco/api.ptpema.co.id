6b5085aaccd8adc83bf7895efff2cff7c81b5023
unknown <hello.safrian@gmail.com>
2024-06-25 15:44:35 +0700

    testing spda data

--- app/Http/Controllers/Vendors/PerusahaanController.php
+++ app/Http/Controllers/Vendors/PerusahaanController.php
@@ -333,7 +333,7 @@ class PerusahaanController extends Controller
 
         $data = ViewPerusahaan::where('user_id', Auth::user()->id)->first();
 
-        $spda = Spda::select('spda.*', 'perusahaan.*', 'perusahaan.id AS id_perusahaan')
+        $spda = Spda::select('spda.*', 'perusahaan.*', 'spda.id AS id_spda', 'perusahaan.id AS id_perusahaan')
                 ->where('id_perusahaan', $data->id)
                 ->join('perusahaan', 'perusahaan.id', '=', 'spda.id_perusahaan')
                 ->get();
