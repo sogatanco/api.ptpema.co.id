14dd86a44bac5af1d45554f3832227258e6292bd
unknown <hello.safrian@gmail.com>
2024-07-07 20:44:52 +0700

    create client model

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Client;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use App\Models\Client\Client;
 
 class ClientController extends Controller
 {
