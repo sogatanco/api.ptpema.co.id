4c4d9e2b02c848c6bf75026a3c9eca3c1cb2e034
unknown <hello.safrian@gmail.com>
2024-07-07 20:08:45 +0700

    testing client controller

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -10,7 +10,7 @@ class ClientController extends Controller
     public function index(Request $request)
     {
 
-        $userRequest = $request->headers();
+        $userRequest = $request->bearerToken();
 
         return response()->json([
             "status" => true,
