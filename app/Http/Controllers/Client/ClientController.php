1f72ac5e649fca3810afa97ed43753c92bb986d6
unknown <hello.safrian@gmail.com>
2024-07-07 20:18:33 +0700

    testing client controller

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -4,17 +4,28 @@ namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ClientController extends Controller
 {
     public function index(Request $request)
     {
+        $query = $request->query('id');
+        $token = $request->bearerToken();
 
-        $userRequest = $request->bearerToken();
+        // if($token){
+            
+        // }else{
+        //     throw new HttpResponseException(response([
+        //         "status" => false,
+        //         "message" => "Unauthenticated."
+        //     ]))
+        // }
 
         return response()->json([
             "status" => true,
-            "message" => $userRequest
+            "message" => $userRequest,
+            "data" => $query
         ], 200);
     }
 }
