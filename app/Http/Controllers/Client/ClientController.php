8bae61d3b91ebfe439821827b9722ec5d73b50a9
unknown <hello.safrian@gmail.com>
2024-07-18 10:11:01 +0700

    testing store employe

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -35,6 +35,8 @@ class ClientController extends Controller
         // create employe as user
         $newUser = new User();
         $newUser->email = $request->email;
+        $newUser->password = bcrypt("asdasdasd");
+        $newUser->role = ["Employee"];
         $newUser->save();
 
         // $newEmploye = Employe::create($request->all());
