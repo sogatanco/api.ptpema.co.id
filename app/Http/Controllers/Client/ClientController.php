5ab09f77532f4375c4a28f0c449889c1c6897090
unknown <hello.safrian@gmail.com>
2024-07-18 14:44:11 +0700

    client position list

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Employe;
 use App\Models\User;
+use App\Models\Position;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
