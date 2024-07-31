e1e422386215282568c23c9d865e05ffc590f9e9
sogatanco <adeliapucha46@gmail.com>
2024-07-31 11:18:09 +0700

    perbaikan create Master Time

--- database/seeders/MasterTime.php
+++ database/seeders/MasterTime.php
@@ -18,18 +18,18 @@ class MasterTime extends Seeder
                 'start' => '00:00:00',
                 'end' => '09:00:00',
                 'rate_pergi' => 1,
-                'rate_pulang' => 0,35,
+                'rate_pulang' => 0.35,
             ],
             [
                 'start' => '09:00:00',
                 'end' => '17:00:00',
-                'rate_pergi' => 0,7,
-                'rate_pulang' => 0,7,
+                'rate_pergi' => 0.7,
+                'rate_pulang' => 0.7,
             ],
             [
                 'start' => '17:00:00',
                 'end' => '24:00:00',
-                'rate_pergi' => 0,35,
+                'rate_pergi' => 0.35,
                 'rate_pulang' => 1,
             ],
         ]);
