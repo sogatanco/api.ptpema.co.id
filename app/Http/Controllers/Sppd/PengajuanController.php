dc1f88cfbd4541aa65b4e7dcd16636d4e7f3e140
sogatanco <adeliapucha46@gmail.com>
2024-08-08 11:45:19 +0700

    sprintf

--- app/Http/Controllers/Sppd/PengajuanController.php
+++ app/Http/Controllers/Sppd/PengajuanController.php
@@ -19,42 +19,43 @@ class PengajuanController extends Controller
     public function store(Request $request)
     {
         $sppd = new Sppd();
-        $sppd->nomor_sppd = ((PenomoranSppd::find($request->nomor)->last_number) + 1) . '/PEMA/ST-' . PenomoranSppd::find($request->nomor)->kode . '/' . $this->getRomawi(date('m')) . '/' . date('Y');
+        $sppd->nomor_sppd = sprintf("%02d", ((PenomoranSppd::find($request->nomor)->last_number) + 1))  . '/PEMA/ST-' . PenomoranSppd::find($request->nomor)->kode . '/' . $this->getRomawi(date('m')) . '/' . date('Y');
         $sppd->nomor_dokumen = unique_random('documents', 'doc_id', 40);
         $sppd->employe_id = $request->employe_id;
         $sppd->nama = $request->name;
         $sppd->jabatan = $request->jabatann;
         $sppd->golongan_rate = $request->rate;
         $sppd->ketetapan =
-        $sppd->submitted_by = Employe::employeId();
+            $sppd->submitted_by = Employe::employeId();
         $sppd->ketetapan = KetetapanSppd::where('status', 'active')->first()->id;
         $tujuans = $request->tujuan_sppd;
-        
+
         if ($sppd->save()) {
+
             for ($i = 0; $i < count($tujuans); $i++) {
-                if($tujuans[$i]['file_undangan']!=='-'){
-                    $file=base64_decode(str_replace('data:application/pdf;base64,', '', $tujuans[$i]['file_undangan']), true);
-                    $fileName='undangan/'.date('Y').'/'.date('m').'/'.date('d').'/'.$sppd->id.'/undangan-'.($i+1).'.pdf';
-                    if(Storage::disk('public_sppd')->put($fileName, $file)){
-                        $file_undangan=$fileName;
-                    }                 
-                }else{
-                    $file_undangan='-';
-                } 
+                if ($tujuans[$i]['file_undangan'] !== '-') {
+                    $file = base64_decode(str_replace('data:application/pdf;base64,', '', $tujuans[$i]['file_undangan']), true);
+                    $fileName = 'undangan/' . date('Y') . '/' . date('m') . '/' . date('d') . '/' . $sppd->id . '/undangan-' . ($i + 1) . '.pdf';
+                    if (Storage::disk('public_sppd')->put($fileName, $file)) {
+                        $file_undangan = $fileName;
+                    }
+                } else {
+                    $file_undangan = '-';
+                }
                 TujuanSppd::insert([
                     'id_sppd' => $sppd->id,
                     'jenis_sppd' => $tujuans[$i]['jenis_sppd'],
                     'dasar' => $tujuans[$i]['dasar_sppd'],
-                    'file_undangan'=>$file_undangan,
+                    'file_undangan' => $file_undangan,
                     'klasifikasi' => $tujuans[$i]['klasifikasi'],
-                    'sumber'=>$tujuans[$i]['sumber_biaya'],
-                    'rkap'=>$tujuans[$i]['renbis'],
+                    'sumber' => $tujuans[$i]['sumber_biaya'],
+                    'rkap' => $tujuans[$i]['renbis'],
                     'p_tiket' => $tujuans[$i]['p_tiket'],
                     'p_um' => $tujuans[$i]['p_um'],
                     'p_tl' => $tujuans[$i]['p_tl'],
                     'p_us' => $tujuans[$i]['p_us'],
                     'p_hotel' => $tujuans[$i]['p_hotel'],
-                    'kategori'=>$tujuans[$i]['kategori_sppd'],
+                    'kategori' => $tujuans[$i]['kategori_sppd'],
                     'detail_tujuan' => $tujuans[$i]['detail_tujuan'],
                     'tugas' => $tujuans[$i]['tugas_sppd'],
                     'waktu_berangkat' => date('Y-m-d H:i:s', strtotime($tujuans[$i]['waktu_berangkat'])),
