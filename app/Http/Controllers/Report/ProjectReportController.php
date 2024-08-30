06969747d5a0b07699e797abab87a27ffedb1477
unknown <hello.safrian@gmail.com>
2024-08-30 10:40:51 +0700

    create report to excel

--- app/Http/Controllers/Report/ProjectReportController.php
+++ app/Http/Controllers/Report/ProjectReportController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Report;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Project;
+use App\Models\Projects\Project;
 
 class ProjectReportController extends Controller
 {
