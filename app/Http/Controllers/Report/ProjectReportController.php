1daeec7b4df1b00f7569bcb2332f72cd13cb505d
unknown <hello.safrian@gmail.com>
2024-08-30 14:23:56 +0700

    create report to excel

--- app/Http/Controllers/Report/ProjectReportController.php
+++ app/Http/Controllers/Report/ProjectReportController.php
@@ -33,6 +33,7 @@ class ProjectReportController extends Controller
     {
         $projects = Project::leftJoin('project_stages', 'project_stages.project_id', '=', 'projects.project_id')
                     ->where('projects.division', 22)
+                    ->where('project_tasks.parent', null)
                     ->get();
 
         return response()->json([
