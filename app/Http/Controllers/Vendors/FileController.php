1086dcb7a54cf2ee78810097742da4905452d749
unknown <hello.safrian@gmail.com>
2024-09-10 12:10:14 +0700

    testing get dokumen2

--- app/Http/Controllers/Vendors/FileController.php
+++ app/Http/Controllers/Vendors/FileController.php
@@ -34,14 +34,14 @@ class FileController extends Controller
         $doc[1]['id']='ktp';
         $doc[1]['file_name']=null;
         $doc[1]['base64']=null;
-        if (file_exists(public_path('vendor_file/' . $p->ktp_pengurus))){
-            $doc[1]['id']='ktp';
-            $doc[1]['file_name']='ktp_pengurus.pdf';
-            $doc[1]['base64']=base64_encode(file_get_contents(public_path('vendor_file/' . $p->ktp_pengurus)));
-        }
+        // if (file_exists(public_path('vendor_file/' . $p->ktp_pengurus))){
+        //     $doc[1]['id']='ktp';
+        //     $doc[1]['file_name']='ktp_pengurus.pdf';
+        //     $doc[1]['base64']=base64_encode(file_get_contents(public_path('vendor_file/' . $p->ktp_pengurus)));
+        // }
 
         return new PostResource(true,'Doc Company', $doc);
-        
+
         $doc[2]['id']='sk_kemenkumham';
         $doc[2]['file_name']=null;
         $doc[2]['base64']=null;
