ea9366bc5a103ffa73737175dad5ec462e6d2781
sogatanco <adeliapucha46@gmail.com>
2024-09-16 22:58:55 +0700

    realisi

---
+++ app/Models/Sppd/RealisasiBiaya.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Sppd;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RealisasiBiaya extends Model
+{
+    protected $connection='mysql4';
+    protected $table='realisasi_biaya';
+}
