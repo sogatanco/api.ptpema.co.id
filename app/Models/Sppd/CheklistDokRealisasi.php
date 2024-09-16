7305fbcc7eccb8b7f81a14465a63fe200111ee12
sogatanco <adeliapucha46@gmail.com>
2024-09-17 03:20:14 +0700

    semoga

---
+++ app/Models/Sppd/CheklistDokRealisasi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Sppd;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CheklistDokRealisasi extends Model
+{
+    protected $connection='mysql4';
+    protected $table='ceklist_dok_realisasi';
+}
