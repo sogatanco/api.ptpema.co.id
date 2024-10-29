ee4e0e95c910e70b9dd95d1f80204ba8540eed61
unknown <hello.safrian@gmail.com>
2024-10-29 14:23:47 +0700

    create download file endpoint

--- app/Http/Controllers/File/FileController.php
+++ app/Http/Controllers/File/FileController.php
@@ -5,18 +5,33 @@ namespace App\Http\Controllers\File;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Vendor\Perusahaan;
 
 class FileController extends Controller
 {
-    public function download()
+    public function download(Request $request)
     {
+        $fileType = $request->query('type');
+        $fileName = $request->query('file');
+
+        $userId = Auth::user()->id;
+        $perusahaan = Perusahaan::where('user_id', $userId)->first();
+
         // file path
         $file = public_path('vendor_file/87/company_profile.pdf');
+        
+        if(fileType != ''){
+            $filePath = public_path('vendor_file/' . $perusahaan->id . '/' . $fileType . '/' . $fileName);
+        }else{
+            $filePath = public_path('vendor_file/' . $perusahaan->id . '/' . $fileName);
+        }
 
         $headers = array(
             'Content-Type: application/pdf',
         );
 
-        return Response::download($file, 'company_profile.pdf', $headers);
+        return Response::download($file, $fileName, $headers);
     }
 }
