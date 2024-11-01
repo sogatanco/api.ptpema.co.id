24dab4c98aa279af8eb1849366fec26f046443fe
unknown <hello.safrian@gmail.com>
2024-11-01 15:06:06 +0700

    add vendorviewer role

--- routes/dapi.php
+++ routes/dapi.php
@@ -76,7 +76,7 @@ Route::controller(APerusahaanController::class)->group(function(){
      Route::get('vendor/log/{id}', 'getLog')->middleware("role:AdminVendorUmum,AdminVendorScm");
      Route::get('vendor/masterkbli', 'list')->middleware("role:AdminVendorUmum,AdminVendorScm");
      Route::get('vendor/companies-to-invite', 'companiesToInvite')->middleware("role:AdminVendorUmum,AdminVendorScm");
-     Route::get('vendor/company-verify-status/{id}', 'getCompanyStatus')->middleware("role:AdminVendorUmum,AdminVendorScm");
+     Route::get('vendor/company-verify-status/{id}', 'getCompanyStatus')->middleware("role:AdminVendorUmum,AdminVendorScm,VendorViewer");
 });
 
 Route::controller(ATenderController::class)->group(function(){
