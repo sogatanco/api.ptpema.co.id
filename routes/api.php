1cbf4190012548c957762a90a52a86a5b4b35dcc
unknown <hello.safrian@gmail.com>
2024-11-04 10:16:49 +0700

    dfsjndf

--- routes/api.php
+++ routes/api.php
@@ -169,5 +169,5 @@ Route::controller(ProjectReportController::class)->group(function(){
 });
 
 Route::controller(PreviewController::class)->group(function(){
-    Route::get('file/preview/{companyId}', 'filePreview')->middleware("role:AdminVendorScm,AdminVendorUmum,VendorViewer");
+    Route::get('file/preview/{companyId}', 'getFile')->middleware("role:AdminVendorScm,AdminVendorUmum,VendorViewer");
 });
