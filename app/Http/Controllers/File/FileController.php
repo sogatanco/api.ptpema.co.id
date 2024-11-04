45c651add3177d22354ed169926fdfc6ccef2cf1
unknown <hello.safrian@gmail.com>
2024-11-04 09:34:28 +0700

    sdkfjns

--- app/Http/Controllers/File/FileController.php
+++ app/Http/Controllers/File/FileController.php
@@ -15,7 +15,7 @@ class FileController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api_vendor');
+        $this->middleware('auth:api_vendor') || $this->middleware('auth:api');
     }
 
     public function download(Request $request)
