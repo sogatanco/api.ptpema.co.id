a6f7bf84c132a5ad63f2a024910d35102c6462e9
sogatanco <adeliapucha46@gmail.com>
2024-11-05 15:37:49 +0700

    tete

--- app/Http/Controllers/Sppd/PengajuanController.php
+++ app/Http/Controllers/Sppd/PengajuanController.php
@@ -332,6 +332,7 @@ class PengajuanController extends Controller
                         'id_tujuan' => $tujuan_realisasi[$i]['id'],
                         'rill_tiket'  => $tujuan_realisasi[$i]['rill_tiket'],
                         'rill_hotel' => $tujuan_realisasi[$i]['rill_hotel'],
+                        'rill_bbm' => $tujuan_realisasi[$i]['rill_bbm'],
                         'rill_wb' =>  date('Y-m-d H:i:s', strtotime($tujuan_realisasi[$i]['rill_wb'])),
                         'rill_wt' => date('Y-m-d H:i:s', strtotime($tujuan_realisasi[$i]['rill_wt'])),
                     ]);
