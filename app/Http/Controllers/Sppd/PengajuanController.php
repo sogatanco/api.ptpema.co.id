eb3a6728add21fab0192965d8601e29080abda7a
sogatanco <adeliapucha46@gmail.com>
2024-11-08 16:20:50 +0700

    gg

--- app/Http/Controllers/Sppd/PengajuanController.php
+++ app/Http/Controllers/Sppd/PengajuanController.php
@@ -103,7 +103,7 @@ class PengajuanController extends Controller
                 $d1->id_unique=(rand(1,100)*$d1->id);
                 $d1->type_proses='uang_muka';
                 // $d1->ddd=gettype($d1);
-                sort((array)$d1);
+                array_unshift((array)$d1, ['dd'=>'dd']);
             }
             $data2 = ListSppd::where('realisasi_status', 'verified')->where('by_keuangan', 0)->get();
             foreach($data2 as $d2){
