8d1804dd1ce2973774ace50c244da6cb6678bb0a
unknown <hello.safrian@gmail.com>
2024-11-13 12:14:14 +0700

    insert org code

--- app/Http/Controllers/Master/OrganizationController.php
+++ app/Http/Controllers/Master/OrganizationController.php
@@ -13,13 +13,25 @@ class OrganizationController extends Controller
         $data = Organization::all();
 
         // generate 4 digit random number
+        $savedCode = [];
+
         for ($i=0; $i < count($data); $i++) { 
             $code = mt_rand(1000, 9999);
 
+            if(!in_array($code, $savedCode)){
+                
+                $theCode = 'ORG'.$code;
+            }else{
+                $otherCode = mt_rand(1000, 9999);
+                $theCode = 'ORG'.$otherCode;
+            }
+
             Organization::where('board_id', $data[$i]['board_id'])
                                 ->update([
-                                    'organization_code' => 'ORG'.$code
+                                    'organization_code' => $theCode
                                 ]);
+
+            array_push($savedCode, $theCode);
         }
 
         return response()->json([
