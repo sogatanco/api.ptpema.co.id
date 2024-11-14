a074b3f60d901a2fdfffbd53ddbfc49fa4aeeee7
unknown <hello.safrian@gmail.com>
2024-11-14 17:20:16 +0700

    create org update

--- app/Http/Controllers/Master/OrganizationController.php
+++ app/Http/Controllers/Master/OrganizationController.php
@@ -33,6 +33,43 @@ class OrganizationController extends Controller
         ], 200);
     }
 
+    public function update(Request $request)
+    {
+        $board = BoardOrganization::where('board_code', $request->board_code)->first();
+
+        $organization = Organization::where('organization_code', $request->organization_code)->first();
+
+        if(!$organization){
+
+           Organization::create([
+                'board_id' => $board->board_id,
+               'organization_code' => $request->organization_code,
+               'organization_name' => $request->organization_name,
+           ]);
+
+        } else {
+
+            $isSaved = Organization::where('organization_code', $request->organization_code)
+                                ->update([
+                                    'board_id' => $board->board_id,
+                                    'organization_name' => $request->organization_name,
+                                ]);
+    
+            if(!$isSaved){
+               throw new HttpResponseException(response([
+                   'status' => false,
+                   'message' => 'Failed to update organization'
+               ], 500));
+            }
+
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Successfully updated organization',
+        ], 200);
+    }
+
     public function insertCode()
     {
         $data = Organization::all();
