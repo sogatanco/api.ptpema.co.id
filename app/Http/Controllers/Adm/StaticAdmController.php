28f69bd4d502c69583e105785758ba3584cd5c1c
sogatanco <adeliapucha46@gmail.com>
2024-11-14 15:02:21 +0700

    sgdsg

---
+++ app/Http/Controllers/Adm/StaticAdmController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Adm;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Employe;
+use App\Models\Organization;
+use App\Models\Structure;
+use Illuminate\Http\Request;
+
+class StaticAdmController extends Controller
+{
+    public function getDivisi()
+    {
+        $data = Organization::where('board_id', Structure::where('employe_id', Employe::employeId())->first()->board_id)->get();
+
+        return new PostResource(true, "Data Divisi", $data);
+    }
+}
