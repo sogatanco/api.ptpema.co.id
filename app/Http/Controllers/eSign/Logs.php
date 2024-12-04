978a9e4a2fbae0fe19f33567a337794d50010f2c
pema-dev <mitrapema@gmail.com>
2024-12-04 15:59:38 +0700

    sdg

--- app/Http/Controllers/eSign/Logs.php
+++ app/Http/Controllers/eSign/Logs.php
@@ -10,8 +10,12 @@ use App\Models\ESign\Log;
 class Logs extends Controller
 {
     public function getLogs($id_doc){
-        $data=Log::where('id_document', $id_doc)->first();
-        $data['first_name']=Structure::where('employe_id', $data->employe_id)->first('first_name')->first_name;
+        $data=Log::where('id_document', $id_doc)->latest()->get();
+        foreach($data as $item){
+            $item['first_name']=Structure::where('employe_id', $item->employe_id)->first('first_name')->first_name;
+            $item['position_name']=Structure::where('employe_id', $item->employe_id)->first('position_name')->position_name;
+
+        }
         return new PostResource(true, 'data logs', $data);
     }
 }
