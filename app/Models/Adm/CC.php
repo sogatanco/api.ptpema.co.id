46a8a8c7ae487df4ac828f6e9036e00490e61e38
pema-dev <mitrapema@gmail.com>
2024-12-13 10:54:28 +0700

    tete

--- app/Models/Adm/CC.php
+++ app/Models/Adm/CC.php
@@ -9,4 +9,10 @@ class CC extends Model
 {
     protected $connection='adm';
     protected $table= 'cc';
+    protected $fillable = [
+        'id_dispo',
+        'cc_to',
+        'id_penerima',
+        'created_at',
+    ];
 }
