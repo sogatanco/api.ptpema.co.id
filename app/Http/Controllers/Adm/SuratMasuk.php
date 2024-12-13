5498ff42d46df5819ee797d6a78fd48f1fe76a66
pema-dev <mitrapema@gmail.com>
2024-12-13 17:03:25 +0700

    www

--- app/Http/Controllers/Adm/SuratMasuk.php
+++ app/Http/Controllers/Adm/SuratMasuk.php
@@ -150,7 +150,7 @@ class SuratMasuk extends Controller
                 'employe_id' => $r->tinjut_by,
                 'position' => $r->tinjut_by_position,
                 'nama' => Structure::where('employe_id', $r->tinjut_by)->first('first_name')->first_name,
-                'activity' => 'Surat ditindak lanjuti dan tidak didisposisi',
+                'activity' => 'Surat ditindaklanjuti dan tidak didisposisi',
                 'cc'=>$cc,
                 'waktu'=>$r->updated_at,
             ]);
