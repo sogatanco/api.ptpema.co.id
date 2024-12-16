ccb0925af864f551bf7fa4481551ea3f8e8ed7af
pema-dev <mitrapema@gmail.com>
2024-12-16 16:35:46 +0700

    eteee

--- config/auth.php
+++ config/auth.php
@@ -109,7 +109,7 @@ return [
             'provider' => 'users',
             'table' => 'password_reset_tokens',
             'expire' => 60,
-            'throttle' => 60,
+            'throttle' => 120,
         ],
     ],
 
