c884b0d0155f078b6800c401c531d85230f65928
unknown <hello.safrian@gmail.com>
2024-12-22 16:14:50 +0700

    asass

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,6 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Fruitcake\Cors\HandleCors::class,
             \Illuminate\Http\Middleware\HandleCors::class,
         ],
         
