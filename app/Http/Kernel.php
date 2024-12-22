2dbdd205ce413ff356f6aa3e86bfe544271df4f2
unknown <hello.safrian@gmail.com>
2024-12-22 16:04:56 +0700

    add 'sso' login

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,8 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Fruitcake\Cors\HandleCors::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
         ],
         
         'dapi' => [
