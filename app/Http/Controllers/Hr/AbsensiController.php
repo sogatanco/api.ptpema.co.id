b61c0646e15c5f9940112b84e071fab7ed420b4e
pema-dev <mitrapema@gmail.com>
2025-03-20 11:16:48 +0700

    sgdgsdg

--- app/Http/Controllers/Hr/AbsensiController.php
+++ app/Http/Controllers/Hr/AbsensiController.php
@@ -18,9 +18,9 @@ class AbsensiController extends Controller
         $formData = [
             'api_key' => 'v9kCmXvvpcb15_QdXr5cVWnq8GWgHCsP',
             'api_secret' => '5RFVIMNXw1KwHP5dF08cM3GWErUZIckL',
-            'image_url1'=>$this->getImage(Employe::employeId()),
+            'image_url1' => $this->getImage(Employe::employeId()),
             // 'image_url2'=>'https://hr-api.ptpema.co.id/storage/photo/employee-photo/CPhefkR6f3b2bQwLn8DCii95LQNNdn1AQq04GeG0.jpg',
-            'image_base64_2'=>$request->poto,
+            'image_base64_2' => $request->poto,
         ];
 
         try {
@@ -29,7 +29,19 @@ class AbsensiController extends Controller
             ]);
 
             $result = json_decode($response->getBody(), true);
-            return response()->json($result, 200);
+
+            // Cek nilai confidence
+            if (isset($result['confidence']) && $result['confidence'] > 85) {
+                return response()->json([
+                    'status' => 'Sesuai',
+                    'confidence' => $result['confidence']
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 'Tidak Sesuai',
+                    'confidence' => $result['confidence'] ?? null
+                ], 200);
+            }
         } catch (RequestException $e) {
             return response()->json([
                 'error' => 'Request error: ' . $e->getMessage()
@@ -37,9 +49,10 @@ class AbsensiController extends Controller
         }
     }
 
-    public function getImage($employe_id){
-        $p=Profil::where('employe_id', $employe_id)->get()->first();
-        $data = 'https://hr-api.ptpema.co.id/storage/photo/employee-photo/'.$p->photo;
+    public function getImage($employe_id)
+    {
+        $p = Profil::where('employe_id', $employe_id)->get()->first();
+        $data = 'https://hr-api.ptpema.co.id/storage/photo/employee-photo/' . $p->photo;
         return $data;
     }
 }
