0371c3cae2996d3b3b248b49ab0ec90ede2797f7
pema-dev <mitrapema@gmail.com>
2025-04-14 15:22:57 +0700

    sdgsdg

--- app/Http/Controllers/MobilOp/MobilController.php
+++ app/Http/Controllers/MobilOp/MobilController.php
@@ -22,7 +22,7 @@ class MobilController extends Controller
     }
 
     public function getMobil() {
-        $data = Mobil::where('status', '=', 'aktif')->get();
+        $data = Mobil::all();
         return new PostResource(true, 'success', $data);
     }
 }
