7b902a0abe7320bdd2c508b7127c2764e155010e
Safrian <safrian@Mac.lan>
2025-04-22 14:52:08 +0700

    add daily update

--- app/Http/Controllers/Daily/DailyController.php
+++ app/Http/Controllers/Daily/DailyController.php
@@ -46,8 +46,43 @@ class DailyController extends Controller
 
         return response()->json([
             "status" => true,
-            "message" => "From daily store controller"
+            "message" => "Successfully created daily."
         ], 200);
 
+    }
+
+    public function update(Request $request){
+        $validator = Validator::make($request->all(), [
+            'daily_id' => ['required'],
+            'activity_name' => ['required'],
+            'start_date' => ['required'],
+            'end_date' => ['required'],
+        ]);
+
+        if($validator->fails()){
+            throw new HttpResponseException(response()->json([
+                "status" => false,
+                "message" => $validator->errors()
+            ], 500));
+        }
+
+        $updated = Daily::where('daily_id', $request->daily_id)->update([
+            'task_id' => $request->task_id,
+            'activity_name' => $request->activity_name,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
+        ]);
+
+        if($updated){
+            return response()->json([
+                "status" => true,
+                "message" => "Successfully updated daily."
+            ], 200);
+        }else{
+            return response()->json([
+                "status" => false,
+                "message" => "Failed to update daily."
+            ], 500);
+        }
     }
 }
